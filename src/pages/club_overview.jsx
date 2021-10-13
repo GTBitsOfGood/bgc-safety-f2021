339170e37cd0d1ae67d656f84d22eb7ca67d21fe
mhartlage3 <madeline.hartlage@gatech.edu>
2021-10-12 22:02:11 -0400

    add basic table and dummy data

--- src/pages/club_overview.jsx
+++ src/pages/club_overview.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import SelectDropdown from "../components/select";
 import urls from "../../utils/urls";
@@ -14,12 +15,29 @@ const useStyles = makeStyles((theme) => ({
     width: "30%",
     borderRadius: "50px",
   },
+  tr: {
+    "&:nth-child(even)": {
+      backgroundColor: "#efefef",
+    },
+  },
+  th: {
+    backgroundColor: "#4da6ff",
+  }
 }));
+
+const ClubName = "Harland"; // TODO: Allow user to select a club
 
 const ClubOverview = () => {
   const classes = useStyles();
   const [routes, setRoutes] = useState([]);
 
+  let regionList = ["East","West","East","West"];
+  let clubList = [
+    ["Harland", regionList[0], "John Smith", "BusA Cap 20/30", "BusB Cap 20/30"],
+    ["Harland",regionList[1], "John Smith", "BusA Cap 20/30", "BusB Cap 20/30"], 
+    ["Harland", regionList[2], "John Smith", "BusA Cap 20/30", "BusB Cap 20/30"], 
+    ["Harland",regionList[3], "John Smith", "BusA Cap 20/30", "BusB Cap 20/30"]
+  ];
   useEffect(() => {
     const getRoutes = async () => {
       const res = await fetch(`${urls.baseUrl}/api/routes`, {
@@ -39,6 +57,32 @@ const ClubOverview = () => {
       <h2>Overview of Clubs</h2>
       <SelectDropdown options={routes} className={classes.dropdown} />
       <Input />
+      
+        <table>
+          <thead>
+            <th
+              className={classes.th}
+            >
+              Club
+              Executive Director
+              Summary
+             </th>
+          </thead>
+          <tbody>
+            {clubList.map((club) => (
+              <tr className={classes.tr}>
+                <td>
+                  {club[0]}
+                  {club[1]}
+                  {club[2]}
+                  {club[3]}
+                  {club[4]}
+                </td>
+              </tr>
+             ))}
+          </tbody>
+        </table>
+      
     </div>
   );
 };
