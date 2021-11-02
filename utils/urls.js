23b7d9e613e0bc310c7a596be7bda78744f6e28b
Kavin Phan <kavin@kphan.tech>
2021-11-02 18:56:01 -0400

    Use origin directly

--- utils/urls.js
+++ utils/urls.js
@@ -1,9 +1,7 @@
 const prod = process.env.NODE_ENV === "production";
 
 export default {
-  baseUrl: prod
-    ? "https://bgcsafety-dev.herokuapp.com"
-    : "http://localhost:3000",
+  baseUrl: window.location.origin,
   dbUrl: prod
     ? process.env.MONGO_DB
     : process.env.MONGO_DEV_DB || "mongodb://localhost:27017",
